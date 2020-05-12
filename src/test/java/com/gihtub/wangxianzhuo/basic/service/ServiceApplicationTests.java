919aa061b74dcf53c96440020574a1d1ca7c2d09
Xian Zhuo <1722741150@qq.com>
2020-05-12 11:24:49 +0800

    feat: 基础spirng boot框架

---
+++ src/test/java/com/gihtub/wangxianzhuo/basic/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihtub.wangxianzhuo.basic.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
